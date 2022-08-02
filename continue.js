8efbe421e30f7c35c315940e4bf58a59004009b9
Ishtiaq <robin.pass36@gmail.com>
2022-08-02 19:53:04 +0600

    again push

--- continue.js
+++ continue.js
@@ -2,7 +2,7 @@ var numbers = [50, 60, 45, 88, 40, 30, 188, 55, 70, 88, 90];
 
 for(var i = 0; i < numbers.length; i++){
     var number = numbers[i];
-    if(number > 40){
+    if(number > 50){
         continue;
     }
     console.log(number);
---
+++ reverse.js
@@ -0,0 +1,23 @@
+/* var number = 1;
+while(number < 10){
+    console.log(number);
+    number += 1;
+} */
+
+/* for(number = 1; number < 10; number++){
+    console.log(number);
+} */
+
+// var number = 10;
+// while(number >= 1){
+//     console.log(number);
+//     number--;
+// }
+
+/* for(number = 10; number >=1; number--){
+    console.log(number);
+} */
+
+for(i = 20; i >= 1; i--){
+    console.log(i);
+}
